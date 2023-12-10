d358576bc1360527e9886e8ea72cfcbd05283286
Anass Bouallaga <anass0bouallaga0@gmail.com>
2023-12-10 12:04:00 +0100

    genesis

---
+++ init.sh
@@ -0,0 +1,6 @@
+pkg install termux-api
+pkg install openssh
+pkg install proot proot-distro
+pkg install git
+proot-distro install ubuntu
+passwd
---
+++ setup.sh
@@ -0,0 +1,1 @@
+proot-distro login ubuntu -- eval "rm -rf pre-compiled-ccm && git clone https://github.com/anass01/pre-compiled-ccm.git && cd pre-compiled-ccm && install.sh"
